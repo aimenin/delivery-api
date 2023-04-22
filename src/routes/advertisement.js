89e91f0faab75fc2f114bb4b5d9b8e663b38758c
aimen <artem.igorevich8@rambler.ru>
2023-04-22 14:21:34 +0300

    add all routes for user and advetisements

--- src/routes/advertisement.js
+++ src/routes/advertisement.js
@@ -1,8 +1,79 @@
 const express = require('express');
 const router = express.Router();
 
+// получение всех объявлений
 router.get('/', (req, res) => {
-  res.json([{ id: 'advertisement' }]);
+  res.json({
+    data: [
+      {
+        id: '507f1f77bcf86cd799439012',
+        shortTitle: 'Продам слона',
+        description: 'kulagin@netology.ru',
+        images: [
+          '/uploads/507f1f77bcf86cd799439011/slon_v_profil.jpg',
+          '/uploads/507f1f77bcf86cd799439011/slon_v_fas.jpg',
+          '/uploads/507f1f77bcf86cd799439011/slon_hobot.jpg',
+        ],
+        user: {
+          id: '507f1f77bcf86cd799439011',
+          name: 'Alex Kulagin',
+        },
+        createdAt: '2020-12-12T10:00:00.000Z',
+      },
+    ],
+    status: 'ok',
+  });
+});
+
+// получение данных одного объявления
+router.get('/:id', (req, res) => {
+  res.json({
+    data: {
+      id: '507f1f77bcf86cd799439012',
+      shortTitle: 'Продам слона',
+      description: 'kulagin@netology.ru',
+      images: [
+        '/uploads/507f1f77bcf86cd799439011/slon_v_profil.jpg',
+        '/uploads/507f1f77bcf86cd799439011/slon_v_fas.jpg',
+        '/uploads/507f1f77bcf86cd799439011/slon_hobot.jpg',
+      ],
+      user: {
+        id: '507f1f77bcf86cd799439011',
+        name: 'Alex Kulagin',
+      },
+      createdAt: '2020-12-12T10:00:00.000Z',
+    },
+    status: 'ok',
+  });
+});
+
+router.post('/', (req, res) => {
+  // аунтификация
+  res.json({
+    data: [
+      {
+        id: '507f1f77bcf86cd799439012',
+        shortTitle: 'Продам слона',
+        description: 'kulagin@netology.ru',
+        images: [
+          '/uploads/507f1f77bcf86cd799439011/slon_v_profil.jpg',
+          '/uploads/507f1f77bcf86cd799439011/slon_v_fas.jpg',
+          '/uploads/507f1f77bcf86cd799439011/slon_hobot.jpg',
+        ],
+        user: {
+          id: '507f1f77bcf86cd799439011',
+          name: 'Alex Kulagin',
+        },
+        createdAt: '2020-12-12T10:00:00.000Z',
+      },
+    ],
+    status: 'ok',
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  // аунтификация
+  res.json({ status: 'ok' });
 });
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/signin', (req, res) => {
-  res.json({ id: 'user' });
+  res.json({
+    data: {
+      id: '507f1f77bcf86cd799439011',
+      email: 'kulagin@netology.ru',
+      name: 'Alex Kulagin',
+      contactPhone: '+7 123 456 78 90',
+    },
+    status: 'ok',
+  });
+});
+
+router.post('/signup', (req, res) => {
+  res.json({
+    data: {
+      id: '507f1f77bcf86cd799439011',
+      email: 'kulagin@netology.ru',
+      name: 'Alex Kulagin',
+      contactPhone: '+7 123 456 78 90',
+    },
+    status: 'ok',
+  });
 });
 
 module.exports = router;
